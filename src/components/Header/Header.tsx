b93993e618b328a9367a707eb78751cab1ff684d
Alisa Promthep <alisa.promthep@gmail.com>
2023-07-08 23:47:08 -0700

    header started, need to style search ba

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import './Header.scss';
+import {BiSearch} from 'react-icons/bi';
+
+const Header = () => {
+    return (
+        <header className='header__container'>
+            <h1 className='header__name'>sticker maker</h1>
+            <div>
+                <form className='searchbar'>
+                    <button className='searchbar__button'>
+                        <BiSearch className='searchbar__icon'/>
+                        </button>
+                    <input
+                    type='text'
+                    className='searchbar__input'
+                    placeholder='Search Sticker'>
+                    </input>
+                </form>
+            </div>
+        </header>
+    )
+}
+
+export default Header
